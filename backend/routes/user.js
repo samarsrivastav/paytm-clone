868c8719fcdd622c3fe5c1b1949c968e41370af0
samarsrivastav <samarsrivastav69@gmail.com>
2024-09-24 20:05:27 +0530

    bug fix User Route

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -15,27 +15,20 @@ const schema=zod.object({
 
 router.get("/bulk",async (req,res)=>{
     const filter=req.body.filter
-    const users=User.find({
-        $or:[
-            {
-                firstName:{
-                    "$regex":filter
-                }
-            },{
-                lastName:{
-                    "$regex":filter
-                }
-            }
+    const users=await User.find({
+        $or: [
+            { firstName: { "$regex": filter } },
+            { lastName: { "$regex": filter} }
         ]
     })
     res.send({
-        user:users.map((user)=>({
+        users: users.map((user) => ({
             username: user.username,
             firstName: user.firstName,
             lastName: user.lastName,
             _id: user._id
         }))
-    })
+    });
 })
 
 router.post("/signup",async (req,res)=>{
